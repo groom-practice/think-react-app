66170d75eaa778a67c52f2c1c0065eab7d2935b1
Yusi <yusi4781@naver.com>
2025-04-17 15:13:20 +0900

    refactor: 컴포넌트 분리

---
+++ src/components/ProductRow.jsx
@@ -0,0 +1,15 @@
+function ProductRow({ product }) {
+  const productName = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+  return (
+    <tr>
+      <td>{productName}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+export default ProductRow;
---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,40 @@
+import ProductCategoryRow from "./ProductCategoryRow";
+import ProductRow from "./ProductRow";
+
+function ProductTable({ products, filterText, inStockOnly }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product, index) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category + index}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name + index} />);
+    lastCategory = product.category;
+  });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+export default ProductTable;
